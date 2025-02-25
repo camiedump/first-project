77ba773191f00f3f53a3cf38126febb500f7e0c1
camille labial <camiedumb@gmail.com>
2025-02-25 21:29:48 +0800

    new update (phase 3)

--- src/components/about-me.jsx
+++ src/components/about-me.jsx
@@ -16,7 +16,7 @@ export default function About() {
       <div className="about-text">
         <h1>About Me</h1>
         <p>
-          Hello! I'm <strong>Camille</strong>, a passionate web designer dedicated to crafting 
+          Hello! I'm <strong>Rosa Camille Labial</strong>, a passionate web designer dedicated to crafting 
           visually stunning and user-friendly websites. My love for technology and creativity 
           drives me to design seamless digital experiences.
         </p>
