b0bdc9fcc54aa9f54d385448dfc2243c74da7e1a
camille labial <camiedumb@gmail.com>
2025-03-05 16:42:51 +0800

    responsive nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,17 +1,30 @@
-import { Link } from "react-router-dom"
-import './hero.css'
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Menu, X } from "lucide-react";
+import "./hero.css";
 
-export default function Nav(){
-  return(
-   <>
-<nav className="Nav">
-<li>
- <Link to="/" className='nav-link'>Home</Link> 
-</li>
-<li>
-  <Link to="/about-me" className='nav-link'>About Me</Link> 
-</li>  
-</nav>
-</>
-  )
+export default function Nav() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className="nav-container">
+      <div className="nav-content">
+        <button onClick={() => setIsOpen(!isOpen)} className="burger-menu">
+          {isOpen ? <X size={28} /> : <Menu size={28} />}
+        </button>
+        <ul className={`nav-links ${isOpen ? "open" : "closed"}`}>
+          <li>
+            <Link to="/" className="nav-link" onClick={() => setIsOpen(false)}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about-me" className="nav-link" onClick={() => setIsOpen(false)}>
+              About Me
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
